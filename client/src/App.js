e99f3ba122b07ecd577ec1741421da53ef4b7e12
joedan138 <joedan138@gmail.com>
2018-06-23 11:58:47 -0600

    login button functions and send to login. then sent to callback page. need to figure out next step from callback page

--- client/src/App.js
+++ client/src/App.js
@@ -7,25 +7,53 @@ import Landing from "./components/pages/Landing";
 import MyBounty from "./components/pages/MyBounty";
 import CreateUser from "./components/pages/CreateUser";
 import HarvestModal from "./components/Modal/HarvestModal";
-import {Container} from "reactstrap";
+import { Container } from "reactstrap";
+import Callback from './Callback/Callback';
+import Auth from './Auth/Auth';
+import history from './history';
+import auth0 from "auth0-js";
 
-const App = () => (
-  
-  <Container fluid>
-  <Router>
+const auth = new Auth();
 
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/mybounty" component={MyBounty} />
-        <Route exact path="/availharvests" component={AvailHarvests} />
-        <Route exact path="/createuser" component={CreateUser} />
-      </Switch>
+const handleAuthentication = ({location}) => {
+  if (/access_token|id_token|error/.test(location.hash)) {
+    auth.handleAuthentication();
+  }
+}
 
-  </Router>
-  </Container>
+class App extends Component {
+constructor(props){
+  super(props)
+  this.state = {};
+}
 
+componentDidMount=()=>{
+  this.setState({test:"this is a test"})
+}
+login() {
+  auth.login()
+}
+changeAppState = (name, value) => {
+  this.setState({[name]:value})
+}
+  render() {
+    return (
+      <Container fluid>
+        <Router  history={history}>
 
-);
+          <Switch>
+            <Route exact path="/" component={() => <Landing auth={auth}  login={this.login} test={this.state.test} />} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/mybounty" component={MyBounty} />
+            <Route exact path="/availharvests" component={AvailHarvests} />
+            <Route exact path="/createuser" component={CreateUser} />
+          </Switch>
+
+        </Router>
+      </Container>
+    );
+  }
+}
+
 
 export default App;
