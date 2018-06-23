b20cfd24e48351f2a85ea0180f78dcd99a870486
Nicole Essex <Essex@nicoleesexsmbp2.uconnect.utah.edu>
2018-06-23 10:04:30 -0600

    Home images

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import {Col, Row, Container } from "reactstrap"
-import "./Home.css"
+import {Col, Row, Container } from "reactstrap";
+import "./Home.css";
+import Image from "./vegbask.jpg";
 // import API from "./utils/API.js";
 // import { Link } from "react-router-dom";
 // import { Col, Row, Container } from "../../components/Grid";
@@ -19,6 +20,7 @@ class Home extends Component {
             <Row>
                 <Col>
                 <h1>My bounty</h1>
+                <img src="vegbask.jpg"/>
                 </Col>
             </Row>
             <Row>
