bc08ae3a7c538f02ca6c106ea0bc06cbb1a23bac
Nicole Essex <Essex@Nicole-Essexs-MacBook-Pro-2.local>
2018-06-24 12:34:15 -0600

    strange things happened with last merge trying to fix it

--- client/src/components/pages/MyBounty/MyBounty.js
+++ client/src/components/pages/MyBounty/MyBounty.js
@@ -12,62 +12,28 @@ WebFont.load({
     }
   })
 
-
-// import DeleteBtn from "../../components/DeleteBtn";
-
-// import API from "../../utils/API";
-// import { Link } from "react-router-dom";
-// import { Col, Row, Container } from "../../components/Grid";
-// import { List, ListItem } from "../../components/List";
-// import { Input, TextArea, FormBtn } from "../../components/Form";
-
 class MyBounty extends Component {
 
     render() {
         return (
-            <div id = "mb-main-container">
-                <HomeButton /> 
-                <Jumbotron className="myBountyContainer">
+          <div id = "mb-main-container">
+            <HomeButton />
+
+            <Jumbotron className="myBountyContainer">
                 <p id="myBountyTitle">Your Crops</p>
                 <div className="popdbinfo">
                 Crops will populate here from database.
                 </div>
-                </Jumbotron>
-                {/* <Jumbotron id="myBountyFormContainer">
-                <FormGroup>
+            </Jumbotron>
+ 
+            <FormGroup className="mbFormGroup">
                 <Label  for="exampleSelect">Category of Harvest</Label>
-                  <Input type="select"    name="select"  id="exampleSelect">
+                <Input type="select"    name="select"  id="exampleSelect">
                     <option>Select One</option>
                     <option>Fruit</option>
                     <option>Vegetable</option>
                     <option>Herbs</option>
-                  </Input>
-                <Label for="exampleDate">Date</Label>
-                <Input type="date" name="date" id="exampleDate" placeholder="date available" />
-                <Label for="exampleTime">Time Available for Drop-by</Label>
-                <span><Input type="time" name="time" id="exampleTime" placeholder="time available"  /></span>
-                <span>Until<Input type="time" name="time" id="exampleTime" placeholder="time available" /></span>
-                <Label for="exampleText">Additional Comments</Label>
-                <Input type="textarea" name="text" id="exampleText" />
-                <div className ="box-label-group">
-                <br/>
-                <Label check>
-                <Input type="checkbox" />{' '}
-                  Ready to Post
-                </Label>
-                </div>
-                </FormGroup>
-                
-                </Jumbotron> */}
-     
-      <FormGroup className="mbFormGroup">
-                <Label  for="exampleSelect">Category of Harvest</Label>
-                  <Input type="select"    name="select"  id="exampleSelect">
-                    <option>Select One</option>
-                    <option>Fruit</option>
-                    <option>Vegetable</option>
-                    <option>Herbs</option>
-                  </Input>
+                </Input>
                 <Label for="exampleDate">Date</Label>
                 <Input type="date" name="date" id="exampleDate" placeholder="date available" />
                 <Label for="exampleTime">Time Available for Drop-by</Label>
@@ -83,30 +49,8 @@ class MyBounty extends Component {
                 </Label></span>
                 <span><AddCropFormButton /></span>
                 </div>
-                </FormGroup>
-   
-            {/* <Container fluid>
-                <Row>
-                    <Col > */}
-                        
-
-                    {/* </Col>
-                 </Row>
-                 <Row>
-                     <Col> */}
-                     
-                    
-                     {/* </Col>
-                     
-                 </Row>
-                 <Row>
-                     <Col>
-                     </Col>
-                 </Row>
-             </Container> */}
-             
-             </div>
-                
+            </FormGroup>
+          </div>
         );
     }
 }
