f00f0746a63dc83b9a61d7100b7660a03f1c9023
jartherogue <jasreev@gmail.com>
2018-07-11 19:28:15 -0600

    noted out base_url

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAccessToken } from './AuthService';
 
 
-const BASE_URL = 'http://localhost:3333';
+// const BASE_URL = 'http://localhost:3333';
 
 export default {
    //gets all crops
