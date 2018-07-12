9aa605b12f97f64f47f6d7a8fe38787eb342f891
jartherogue <jasreev@gmail.com>
2018-07-11 19:19:32 -0600

    auth

--- client/src/utils/AuthService.js
+++ client/src/utils/AuthService.js
@@ -6,7 +6,7 @@ const ACCESS_TOKEN_KEY = 'access_token';
 
 const CLIENT_ID = 'rq_IMexCwKdEc9vdpoRN5YGZ8yo_1SNq';
 const CLIENT_DOMAIN = 'metro-agro.auth0.com';
-const REDIRECT = 'http://localhost:3000/callback';
+const REDIRECT = window.location;
 const SCOPE = 'YOUR_SCOPE';
 const AUDIENCE = 'https://metro-agrov2.com';
 
